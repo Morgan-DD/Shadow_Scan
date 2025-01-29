59d02c2c839e7651ee387f79945538366ffbe58e
Morgan-DD <111990474+Morgan-DD@users.noreply.github.com>
2025-01-29 12:05:58 +0100

    client side for test done

---
+++ code/Server(prof)/ShadowScan_Server/Program.cs
@@ -0,0 +1,30 @@
+﻿using Grpc.Net.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using System.Net.Http;
+using ShadowScan_Client;
+
+namespace ShadowScan_Server
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var input = new HelloRequest { Name = "LeBogoss" };
+
+            var channel = GrpcChannel.ForAddress("http://INF-A23-P208:55052");
+            var client = new Greeter.GreeterClient(channel);
+
+            var reply = await client.SayHelloAsync(input);
+
+            Console.WriteLine(reply);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ code/Server(prof)/ShadowScan_Server_1/Program.cs
@@ -0,0 +1,39 @@
+﻿using Grpc.Net.Client;
+using ShadowScan_Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using System.Net.Http;
+
+namespace ShadowScan_Server
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var input = new HelloRequest { Name = "LeBoss" };
+
+            var httpHandler = new SocketsHttpHandler
+            {
+                PooledConnectionLifetime = TimeSpan.FromMinutes(5) // Optional: Helps with DNS changes
+            };
+
+            var channel = GrpcChannel.ForAddress("https://your-grpc-server", new GrpcChannelOptions
+            {
+                HttpHandler = httpHandler
+            });
+            var client = new Greeter.GreeterClient(channel);
+            var response = await client.SayHelloAsync(new HelloRequest { Name = ".NET" });
+
+            var reply = await client.SayHelloAsync(input);
+
+            Console.WriteLine(reply.Message);
+
+            Console.ReadLine();
+        }
+    }
+}
