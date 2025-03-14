f8f8c78a0c0596ce7ce1f16d34c9cbb01f541110
Morgan-DD <111990474+Morgan-DD@users.noreply.github.com>
2025-03-14 10:25:06 +0100

    doc and cleaning

--- code/Server(prof)/GUI_server/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections.Generic;
-
-public class JsonManager
-{
-    public List<string> Ressources { get; set; }
-    // public List<string> type_2 { get; set; }
-    // public List<string> type_3 { get; set; }
-}
